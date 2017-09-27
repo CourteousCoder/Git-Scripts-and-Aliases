e03eee97535fc69532b0291534419ae596961022
Daniel Schetritt <Daniel.Schetritt@live.com>
2017-09-27 11:51:29 -0700

    Wrote adding and committing functionality

--- git-done.py
+++ git-done.py
@@ -6,4 +6,22 @@ import subprocess
 # a comit, commits the changes with an interactive message prompt, and
 # repeats until there are no more changes. Then it pushes the branch.
 
+def run(command):
+    return subprocess.run(command.split(' '))
+
+def git_status():
+    status = subprocess.run("git status --porcelain".split(' '),stdout=subprocess.PIPE)
+    if status.stdout:
+        return status.stdout.decode('utf-8')
+    else:
+        return ''
+
+# Main Script
+
+status = git_status()
+while status:
+    run('git add -p .')
+    run('git commit')
+    print(git_status())
+    exit(0)
 

aeb1f5590987b6e0c632436f4a4dea29f04c0e65
Daniel Schetritt <Daniel.Schetritt@live.com>
2017-09-27 11:49:34 -0700

    Started writing the script.

--- git-done.py
+++ git-done.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+import subprocess
+
 # This script allows you to interactively add portions of your changes to
-# a comit, commits the changes with an interactive message prompt, and 
+# a comit, commits the changes with an interactive message prompt, and
 # repeats until there are no more changes. Then it pushes the branch.
 
 
